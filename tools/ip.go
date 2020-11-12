05bd2702efdba3f3de05006a32615e18a7427205
YuleiLan <yllan@aibee.com>
2020-11-12 14:48:27 +0800

    feat: 添加是否获取地理位置的开关。

--- tools/ip.go
+++ tools/ip.go
@@ -5,29 +5,33 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/spf13/viper"
 )
 
 func GetLocation(ip string) string {
-	if ip == "127.0.0.1" || ip == "localhost" {
-		return "内部IP"
-	}
-	resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
-	if err != nil {
-		panic(err)
+	var address = "已关闭位置获取"
+	if viper.GetBool("settings.public.isLocation") {
+		if ip == "127.0.0.1" || ip == "localhost" {
+			return "内部IP"
+		}
+		resp, err := http.Get("https://restapi.amap.com/v3/ip?ip=" + ip + "&key=3fabc36c20379fbb9300c79b19d5d05e")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+		s, err := ioutil.ReadAll(resp.Body)
 
-	}
-	defer resp.Body.Close()
-	s, err := ioutil.ReadAll(resp.Body)
-	fmt.Printf(string(s))
+		m := make(map[string]string)
 
-	m := make(map[string]string)
-
-	err = json.Unmarshal(s, &m)
-	if err != nil {
-		fmt.Println("Umarshal failed:", err)
+		err = json.Unmarshal(s, &m)
+		if err != nil {
+			fmt.Println("Umarshal failed:", err)
+		}
+		if m["province"] == "" {
+			return "未知位置"
+		}
+		address = m["province"] + "-" + m["city"]
 	}
-	if m["province"] == "" {
-		return "未知位置"
-	}
-	return m["province"] + "-" + m["city"]
+	return address
 }
